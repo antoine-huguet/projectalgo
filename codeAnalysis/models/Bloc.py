ba5c2070f43460c33ae736d6f534f4d62127e739
Irhboula Youssef <youssef.irhboula@student-cs.fr>
2019-11-18 10:42:20 +0100

    latest updates of bloc and config

--- codeAnalysis/models/Bloc.py
+++ codeAnalysis/models/Bloc.py
@@ -1,10 +1,12 @@
-prefixes=['if','while','print(','=','']
-suffixes=[':\n',':\n',')\n',]
-class bloc:
-    def __init__(self,type,tab,prefix,suffix='\n',condition=False,argument=[None,None]):
+import config
+class Bloc:
+    def __init__(self,type,tab,prefix,suffix,condition=False,argument=[None,None]):
         self.type=type
-        self.tab=tab
-        self.prefix=prefix
+        self.tab=tabs[i]
+        self.prefix=prefixes[i]
+        self.suffix=suffixes[i]
+#argument est une liste qui contient les éventuels arguments du bloc: [nom,valeur] ou [None,printable]
+
 
 def code_executable(block_list):
     code=[]
@@ -17,9 +19,10 @@ def code_executable(block_list):
         code.append(txt.(bloc.condition()))
         code.append(bloc.suffixe)
         t=t+bloc.tab
+    return(code.join())
 
 def code_utilisateur(block_list):
-    code=[]
+    code=['from time import * \n']
     t=0
     for bloc in blocklist:
         code.append(t*'\t')
@@ -31,5 +34,5 @@ def code_utilisateur(block_list):
         t=t+bloc.tab
         if bloc.prefixe=='=':
             code.append(affiche(code.argument))
-            code.append('\npause(.3)\n')
-
+            code.append('\ntime.pause(.3)\n')
+    return(code.join())
--- codeAnalysis/models/config.py
+++ codeAnalysis/models/config.py
@@ -1,3 +1,6 @@
 '''This file is used to initialize all global variables.'''
+prefixes=['if','while','print(','=','']
+suffixes=[':\n',':\n',')\n',]
+tabs=[1,1,0,0,-1]
 
 variableId = 0
