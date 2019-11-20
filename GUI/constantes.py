96f2efa8d7e0bfaa5b7d5ef67e3c3dde29009020
Antoine_HUGUET <antoine.huguet@student-cs.fr>
2019-11-20 09:47:38 +0100

    Refracting constantes.py

--- GUI/constantes.py
+++ GUI/constantes.py
@@ -1,27 +1,39 @@
 screen_width = 960
 screen_height = 540
 
-IF_path = 'GUI/resources/blocks/IF.png'
-START_path = 'GUI/resources/blocks/START.png'
+x_spacing = 5
+y_spacing = 10
+
 BG_generic_path = 'GUI/resources/backgrounds/generic.png'
-IF_SNAP_path = 'GUI/resources/blocks/IF_SNAP.png'
+
+#Start
+START_path = 'GUI/resources/blocks/start.png'
+
+#Snap
+SNAP_path = 'GUI/resources/blocks/snap.png'
+
+#Controle
+IF_path = 'GUI/resources/blocks/IF.png'
+ELSE_path = 'GUI/resources/blocks/else.png'
+END_path = 'GUI/resources/blocks/end.png'
+WHILE_path = 'GUI/resources/blocks/while.png'
+
+#Operators
 MINUS_path = 'GUI/resources/blocks/-.png'
 PLUS_path = 'GUI/resources/blocks/+.png'
+PL_path = 'GUI/resources/blocks/pL.png'
+PR_path = 'GUI/resources/blocks/pR.png'
+DIV_path = 'GUI/resources/blocks/div.png'
+X_PATH = 'GUI/resources/blocks/x.png'
+
+#Variables
 A_path = 'GUI/resources/blocks/A.png'
-AFFECTATION_path = 'GUI/resources/blocks/affectation.png'
 B_path = 'GUI/resources/blocks/B.png'
 C_path = 'GUI/resources/blocks/C.png'
 D_path = 'GUI/resources/blocks/D.png'
-DIV_path = 'GUI/resources/blocks/div.png'
 E_path = 'GUI/resources/blocks/E.png'
-ELSE_path = 'GUI/resources/blocks/else.png'
-END_path = 'GUI/resources/blocks/end.png'
 F_path = 'GUI/resources/blocks/F.png'
-PL_path = 'GUI/resources/blocks/pL.png'
-PR_path = 'GUI/resources/blocks/pR.png'
-PRINT_path = 'GUI/resources/blocks/print.png'
-WHILE_path = 'GUI/resources/blocks/while.png'
-X_PATH = 'GUI/resources/blocks/x.png'
 
-x_spacing = 5
-y_spacing = 10
+#Divers
+AFFECTATION_path = 'GUI/resources/blocks/affectation.png'
+PRINT_path = 'GUI/resources/blocks/print.png'
