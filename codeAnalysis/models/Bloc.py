416aae6bb090163980ef471b5cbf4a1cd2e2fb4d
Irhboula Youssef <youssef.irhboula@student-cs.fr>
2019-11-20 10:35:34 +0100

    added display function, pauses in the user code, and Calcul class

--- codeAnalysis/models/Bloc.py
+++ codeAnalysis/models/Bloc.py
@@ -9,6 +9,34 @@ class Bloc:
             self.suffix=str(args[1])+')'
         self.condition=condition
         self.args=args
+
+dico_calcul={'.':'*','*':'*','/':'/', '%':'%','^':'**', '²':'**2'}
+
+
+class Calcul_string(Bloc):
+    def __init__(self,uses_variables,text,code=''):
+        super().__init__(Bloc)
+        self.uses_variables=uses_variables
+        self.text=text
+    def python(self):
+        txt=self.text
+        for i in range(len(txt)):
+            if txt[i] in dico_calcul:
+                self.code+=dico_calcul[txt[i]] 
+            if 47<ord(txt[i])<58:
+                self.code+=txt[i]
+            if i>1 and 47<ord(txt[i-1])<58:
+                self.code+='*'+txt[i]
+            if ord(txt[i])>64 and ord(txt[i+1])>64:
+                self.code+=txt[i]+'*'
+        return self.code
+    def evaluate(self):
+        exec(python.self())
+
+            
+
+
+
 #argument est une liste qui contient les éventuels arguments du bloc: [nom_variable,valeur] ou [None,printable]
 #tab est la tabulation relative après lecture de ce bloc
 
@@ -28,7 +56,7 @@ def code_executable(block_list):
     return(''.join(code))
 
 def code_utilisateur(block_list):
-    code=[]
+    code=['from time import sleep']
     L=[]
     t=0
     for row in blocklist:
@@ -43,14 +71,21 @@ def code_utilisateur(block_list):
             t=t+bloc.tab
             if bloc.id==3 or bloc.id==4:
                 L.append(bloc.args)
+                code.append('\n time.sleep(.2) \n display(bloc.args)')
             #mise à jour de la liste des affectations et prints
         code.append('\n')
     return(''.join(code),L)
 
+def display(args):
+    if args[0]==None:
+        display_print(args[1])
+    else:
+        if args[1].uses_variables:
+            string= str(args[0])+'='+args[1].text +'=' +str(evaluate.args[1])
+        else:
+            string= str(args[0])+'='+args[1].text
+        display_affectation(string)
+
+
 blocklist=[[Bloc(5),Bloc(3,args=('a',1))],[Bloc(6),Bloc(3,args=('b',2))],[Bloc(7),Bloc(3,args=('c',1))],[Bloc(8),Bloc(3,args=('d','b*b-4*a*c'))],[Bloc(2,args=(None,'d'))]]
 #code qui print le discriminant de aX²+bX+c
-
-file=open('code.py','w+')
-file.write(code_utilisateur(blocklist)[0])
-file.close()
-#écriture du code associé à blocklist dans code.py
