c2e9df4f5ed757ace3fe87770133739bb0a61fa4
Irhboula Youssef <youssef.irhboula@student-cs.fr>
2019-11-21 09:54:00 +0100

    added tests file and frontend backend integration file

---
+++ codeAnalysis/tests.py
@@ -0,0 +1,4 @@
+from unittest import *
+
+blocklist=[[Bloc(5),Bloc(3,args=('a',1))],[Bloc(6),Bloc(3,args=('b',2))],[Bloc(7),Bloc(3,args=('c',1))],[Bloc(8),Bloc(3,args=('d','b*b-4*a*c'))],[Bloc(2,args=(None,'d'))]]
+#code qui print le discriminant de aX²+bX+c
