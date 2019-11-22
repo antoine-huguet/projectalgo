6d5b014e0f4150e8f6328b851036aaec54ab392d
Heitz Remi <remi.heitz@student-cs.fr>
2019-11-22 10:23:02 +0100

    Cleanup

--- codeAnalysis/tests.py
+++ codeAnalysis/tests.py
@@ -35,9 +35,3 @@ class TestStringMethods(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
-'''
-blocklist=[[Bloc(5),Bloc(3,args=('a',1))],[Bloc(6),Bloc(3,args=('b',2))],[Bloc(7),Bloc(3,args=('c',1))],[Bloc(8),Bloc(3,args=('d',Calculstring('b²-4ac')))],[Bloc(2,args=(None,'d'))]]
-'''
-#code qui print le discriminant de aX²+bX+c
-#code=1
